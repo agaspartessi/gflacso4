de1c1b35b5948759ab3f54c5caa1087790eacb20
Gaspar Tessi <agaspartessi@gmail.com>
2023-01-26 15:40:35 -0300

    cambiando config probando branch

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+// VERSION para 4.0
 // Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
 
 // This line protects the file from being accessed by a URL directly.                                                               
